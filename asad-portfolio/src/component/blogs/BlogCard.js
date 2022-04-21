095900296f578f7a04d89fdd881e64711d9c0799
Asad Maraka <asadmaraka@Asads-MacBook-Air.local>
2022-04-21 18:59:01 +0300

    starting assignment 2

--- asad-portfolio/src/component/blogs/BlogCard.js
+++ asad-portfolio/src/component/blogs/BlogCard.js
@@ -3,10 +3,10 @@ import "../../sass/_main.scss"
 import ButtonMain from '../buttonMain/ButtonMain';
 import { Link } from 'react-router-dom'
 
-const BlogCard = ({id,title,description, picture,show}) => {
+const BlogCard = ({id,title,description, picture,alt,show}) => {
   return (
     <article className={show==false?"blog__article":"blogs__article"} >
-      <img src={picture} alt="Keyboard with blue lights"  className="blogs__image" />
+      <img src={picture} alt={alt}  className="blogs__image" />
       <h3 id={id} class="blogs__h3">{title}</h3>
       <p class="blogs__p">{show==false?description: description.substring(1,90)} ...</p>
       {show==false ? null :<Link to={`${id}`}> <ButtonMain title={title} description={description} pic={picture}/></Link>}
--- asad-portfolio/src/component/form/Form.js
+++ asad-portfolio/src/component/form/Form.js
@@ -3,18 +3,18 @@ import "../../sass/_main.scss"
 const Form = () => {
   return (
 
-    <section class="form" id="letsTalk">
-      <h2 class="form__h2">Lets talk</h2>
-      <form>
-        <fieldset class="form__fieldset">
+    <section className="form" id="letsTalk">
+      <h2 className="form__h2">Lets talk</h2>
+      <form className='form__element'>
+        <fieldset className="form__fieldset">
           <label for="fname">Name</label>
-          <input type="text" name="fname" id="fname" class="form__input" placeholder="First Name" />
+          <input type="text" name="fname" id="fname" className="form__input" placeholder="First Name" />
           <label for="lname">Second input</label>
-          <input type="text" name="lname" id="lname" class="form__input" placeholder="Last Name" />
+          <input type="text" name="lname" id="lname" className="form__input" placeholder="Last Name" />
           <label for="Message">Message</label>
           <textarea name="Message" id="Message" cols="30" rows="10" placeholder="Message"
-            class="form__message" />
-          <input type="submit" value="Submit" class="btn" />
+            className="form__message" />
+          <input type="submit" value="Submit" className="btn" />
         </fieldset>
       </form>
     </section>
